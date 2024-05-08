062116033bc028eaf073457cec472ee8b46a2f48
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-05-08 13:47:21 +0530

    connection made

--- frontend/client side/src/App.tsx
+++ frontend/client side/src/App.tsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [socket, setSocket] = useState<WebSocket | null>(null);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [, setSocket] = useState<WebSocket | null>(null);
+  const [latestMessage, setlatestMessage] = useState('');
 
   useEffect(() => {
     const newSocket = new WebSocket('ws://localhost:8080');
     newSocket.onopen = () => {
       console.log('Connection established');
-      newSocket.send('Hello Server!');
+     // newSocket.send('Hello Server!');
     }
     newSocket.onmessage = (message) => {
       console.log('Message received:', message.data);
+      setlatestMessage(message.data)
     }
     setSocket(newSocket);
     return () => newSocket.close();
@@ -20,6 +23,7 @@ function App() {
   return (
     <>
       hi there
+      {latestMessage}
     </>
   )
 }
